48efc9dc99b8fbd0301aeb41304c31a232fceb35
nick klosterman <nick_klosterman@yahoo.com>
2014-05-26 22:36:52 -0400

    emits data entered into mongo db via file watch

--- emitMongoFileWatch/app.js
+++ emitMongoFileWatch/app.js
@@ -13,6 +13,91 @@ var app = http.createServer(function(req, res) {
 // Socket.io server listens to our app
 var io = require('socket.io').listen(app);
 
+//we only need this for startup bc otherwise we trigger on an insert/change to the db and the last record will be the one most recently entered
+function getLastRecordForSite(site) {
+    mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
+        if (err) {throw err;}
+        var collection  = db.collection('deal');
+//        collection.find({site:site}).sort({_id:-1}).limit(1).toArray(function(err,data) {
+        collection.find({site:site}).limit(1).toArray(function(err,data) {
+	    console.log('getLastRecordForSite '+site);
+                switch(site){
+                case "WM":
+                case "CL":
+//		    console.log(data.productTitle);
+		    console.log(data);
+		    break;
+		case "SAC":
+//		    console.log(data.name);
+		    console.log(data);
+		    break;
+		}
+	});//end collection.find
+//	db.close();
+    });//end mongoClient.connect
+
+};
+
+//we trigger on an insert/change to the db and the last record will be the one most recently entered
+function getLastRecord() {
+    mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
+        if (err) {throw err;}
+        var collection = db.collection('deal');
+        collection.find({}).sort({_id:-1}).limit(1).toArray(function(err,data) {
+	    if (err) { console.log(err); }
+	    console.log('getLastRecord');
+	    if (typeof data !== 'undefined' && data !== null && data.length > 0) {
+                switch(data[0].site){
+                case "WM":
+                case "CL":
+		    console.log('getlastRecord====:'+data[0].productTitle);
+		    getRecordListForProduct(data[0].productTitle);
+		    break;
+		case "SAC":
+		    console.log('getLastRecord----:'+data[0].name);
+		    getRecordListForProduct(data[0].name);
+		    break; 
+		default:
+		    console.log('getLastRecord----:'+JSON.stringify(data));
+		    break;
+		}
+	    } else {
+		console.log('fuck');
+	    }
+	});//end collection.find
+//	db.close();
+    });//end mongoClient.connect
+    console.log('end getLastRecord');
+};
+
+function getRecordListForProduct(product) {
+    var searchProduct = product;
+    mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
+        if (err) {throw err;}
+        //deals will be the name of the table/colleciton                                         
+        var collection  = db.collection('deal');
+	collection.find({ $or: [ {name: searchProduct },{productTitle: searchProduct }]}).toArray(function(err,data) {
+	    console.log('getRecordListForProduct:');
+            for (var counter = 0, size=data.length; counter<size;counter++){
+                switch(data[counter].site){
+                case "WM":
+                case "CL":
+		    console.log(data[counter].productTitle+' '+data[counter].price);
+		    break;
+		case "SAC":
+		    console.log(data[counter].name+' '+data[counter].price);
+		    break;
+		default:
+		    console.log('site not found');
+		}
+	    }
+
+	});//end collection.find
+//	db.close();
+    });//end mongoClient.connect
+
+};
+
 function getRecordsFromDB(site,callback) {
     var output;
     mongoClient.connect('mongodb://127.0.0.1:27017/bc',function(err,db){
@@ -22,7 +107,10 @@ function getRecordsFromDB(site,callback) {
         output="<ul>";
         var image;
 	//console.log(site);
-        collection.find({site:site}).sort({_id:-1}).limit(14).toArray(function(err,data) {
+        collection.find({site:site}).sort({_id:-1}).limit(5).toArray(function(err,data) {
+	
+//        collection.find({}).sort({_id:-1}).limit(5).toArray(function(err,data) {
+//console.log(data);
             if (err) { throw err;}
             else {
                 switch(site){
@@ -75,41 +163,59 @@ function getRecordsFromDB(site,callback) {
                     break;
                 }
             }
-            db.close();//node will hang without this
+//            db.close();//node will hang without this
             output+="</ul>";
-	    console.log("site:"+site);
-	    console.log("ouput:"+output);
+//	    console.log("site:"+site);
+//	    console.log("ouput:"+output);
             console.log("finish up getRecordsFromDB");
 
             callback( site,output );
         });
     });
 };
-
+var increment=0;
 function emitData(site,data) {
     console.log('emitData called for '+site);
+//    console.log('data:' + data);
     io.sockets.emit(site ,{data:data});
+    var time = new Date().getTime();
+//    io.sockets.emit(site,{data:time});
+    io.sockets.emit('counter', {counter:increment});
 };
 
 var counter=0; 
 // Emit welcome message on connection
 io.sockets.on('connection', function(socket) {
     console.log('connection');
+
+//get initial data set so they aren't staring at an empty screen.
+    getRecordsFromDB('SAC',emitData);
+//    getLastRecordForSite('SAC');
+
+    getRecordsFromDB('CL',emitData);
+  //  getLastRecordForSite('CL');
+
+    getRecordsFromDB('WM',emitData);
+//   getLastRecordForSite('WM');
+   getLastRecord();
+
+    //watch out 'heartbeat' file for notifications of any changes
     fs.watch('/tmp/BackCountryHeartbeat.txt',function(curr,prev) { 
 	var options={encoding:'utf8'};
 	console.log('file change detected');
 	fs.readFile('/tmp/BackCountryHeartbeat.txt',options,function(err,data) { 
-	    
+	    //extract site from the watch file
 	    var site = data.split(',')[0];
 	    console.log(site);
 	    //for some reason this is being called twice.
 	    if (typeof site !== 'undefined' && site !== "") {
 		getRecordsFromDB(site,emitData);
+		getLastRecord();
 	    }
 	})
     });
     
-    socket.emit('welcome', { message: 'Welcome!' });
+    socket.emit('welcome', { message: 'Welcome! You are user #'+counter });
     counter++;
     console.log('connect, num users:'+counter);
     
